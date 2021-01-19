803a02e7042af91d1a2c4fb79e18c9b309c3883e
Michael Wang <michael.wang@myob.com>
2021-01-19 17:43:07 +1100

    Added tests for RoverController, created RoverController class and IRoverCommand interface

---
+++ MarsRover.Tests/RoverControllerTests.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using Moq;
+using Xunit;
+
+namespace MarsRover.Tests
+{
+    public class RoverControllerTests
+    {
+        [Fact]
+        public void ExecuteInputCommands_ShouldExecuteTheRelevantRoverCommands()
+        {
+            var mockRoverCommand = new Mock<IRoverCommand>();
+            var mockCommandsList = new List<IRoverCommand>()
+            {
+                mockRoverCommand.Object
+            };
+            var mockUserInput = new List<char>()
+            {
+                'r'
+            };
+            var roverController = new RoverController(mockCommandsList);
+
+            mockRoverCommand.Setup(x => x.KeyCommand).Returns('r');
+
+            roverController.ExecuteInputCommands(mockUserInput);
+
+            mockRoverCommand.Verify(x => x.Execute(), Times.Exactly(1));
+        }
+    }
+}
