b77db47c21155959ad276f1addc8d79e69003af9
Michael Wang <michael.wang@myob.com>
2021-01-21 01:58:35 +1100

    Added test for GridDisplay. Created GridDisplay class

---
+++ MarsRover.Tests/GridDisplayTests.cs
@@ -0,0 +1,27 @@
+using Moq;
+using Xunit;
+
+namespace MarsRover.Tests
+{
+    public class GridDisplayTests
+    {
+        [Fact]
+        public void GetGridAsString()
+        {
+            var grid = new Grid(4,4);
+            var mockRover = new Mock<IRover>();
+            var roverLocation = grid.Find(1,1);
+            var obstacleLocation = grid.Find(2,2);
+            obstacleLocation.SquareState = SquareState.Not_Empty;
+            var gridView = new GridDisplay(grid, mockRover.Object);
+            var expected = "\n🟥🟥🟥🟥\n🟥🟥🟥🟥\n🟥🌋🟥🟥\n⏩🟥🟥🟥";
+
+            mockRover.Setup(x => x.CurrentFacingDirection).Returns(new FacingEast());
+            mockRover.Setup(x => x.CurrentSquareLocation).Returns(roverLocation);
+            
+            var actual = gridView.GetGridAsString();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ MarsRover/GridDisplay.cs
@@ -0,0 +1,63 @@
+namespace MarsRover
+{
+    public class GridDisplay
+    {
+        private IGrid _grid;
+        private IRover _rover;
+
+        public GridDisplay(IGrid grid, IRover rover)
+        {
+            _grid = grid;
+            _rover = rover;
+        }
+        public string GetGridAsString()
+        {
+            var gridString = "";
+            var gridRow = "";
+            foreach(ISquare square in _grid.Squares)
+            {
+                if(square.Column < _grid.Columns)
+                {
+                    gridRow += GetIcon(square);
+                }
+                else
+                {
+                    gridRow += GetIcon(square);
+                    gridString = "\n" + gridRow + gridString;
+                    gridRow = "";
+                }
+
+            }
+            return gridString;
+        }
+        
+        private string GetIcon(ISquare square)
+        {
+            if(_rover.CurrentSquareLocation.Equals(square))
+            {
+                switch(_rover.CurrentFacingDirection.Name)
+                {
+                    case DirectionName.North:
+                        return "🔼";
+                    case DirectionName.East:
+                        return "⏩";
+                    case DirectionName.South:
+                        return "🔽";
+                    case DirectionName.West:
+                        return "⏪";
+                }
+            }
+            
+            if(square.SquareState.Equals(SquareState.Empty))
+            {
+                return "🟥";
+            }
+
+            if(square.SquareState.Equals(SquareState.Not_Empty))
+            {
+                return "🌋";
+            }
+            return "";
+        }
+    }
+}
